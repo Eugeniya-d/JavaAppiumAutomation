2328c72825b817ce590c287a4fc48ffe0a3fe493
Eugeniya-d <edavydova1992@gmail.com>
2021-03-18 22:03:58 +0300

    Подбор кнопок до Back на странице статьи

--- src/ui/ios/IOSArticlePageObject.java
+++ src/ui/ios/IOSArticlePageObject.java
@@ -6,8 +6,8 @@ import ui.factories.ArticlePageObjectFactory;
 
 public class IOSArticlePageObject extends ArticlePageObject {
     static {
-        ARTICLE_TO_FAVORITE_LIST_BUTTON = "xpath://ui.android.widget.LinearLayout/ui.android.support.v7.app.ActionBar.Tab/ui.android.widget.ImageView[@content-desc='Add this article to a reading list']";
-        CONFIRM_ARTICLE_SELECTION_BUTTON = "xpath://*[contains(@text, 'GOT IT')]";
+        ARTICLE_TO_FAVORITE_LIST_BUTTON = "xpath://XCUIElementTypeButton[@name='save']";
+        CONFIRM_ARTICLE_SELECTION_BUTTON = "xpath://XCUIElementTypeButton[@name='places auth close']";
         STRING_NAME_OF_FOLDER = "id:org.wikipedia:id/text_input";
         CONFIRM_ARTICLE_ADDICTION = "xpath://*[contains(@text, 'OK')]";
         ADD_ARTICLE_TO_EXISTING_FOLDER_TPL = "xpath://*[contains(@text, '{NAME_OF_FOLDER}')]";
@@ -17,3 +17,4 @@ public class IOSArticlePageObject extends ArticlePageObject {
         super(driver);
     }
 }
+
